3fbdaef32766720bdedba4f154d906b4f858280d
Karan Preet Singh Sasan <preetkaran20@gmail.com>
2021-05-15 13:28:48 +0530

    Fixing

--- lua-modules/vulnerableapp_utility.lua
+++ lua-modules/vulnerableapp_utility.lua
@@ -1,19 +1,19 @@
 local vulnerableapp_utility = {}
 
-function vulnerableapp_utility.merge_vulnerability_information(vulnerableAppResponse, vulnerableAppJspResponse)
+function vulnerableapp_utility.merge_vulnerability_information(vulnerableAppResponse)
     local response = "{"
     local appendComma = false
     if (vulnerableAppResponse.status == 200) then
         response = response .. '"VulnerableApp":' .. vulnerableAppResponse.body
         appendComma = true
     end
-    if (vulnerableAppJspResponse.status == 200) then
-        if (appendComma) then
-            response = response .. ","
-        end
-        appendComma = true
-        response = response .. '"VulnerableApp-jsp":' .. vulnerableAppJspResponse.body
-    end
+    #if (vulnerableAppJspResponse.status == 200) then
+    #    if (appendComma) then
+    #        response = response .. ","
+    #    end
+    #    appendComma = true
+    #    response = response .. '"VulnerableApp-jsp":' .. vulnerableAppJspResponse.body
+    #end
     response = response .. "}"
     return response
 end
