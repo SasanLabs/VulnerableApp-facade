f8024a3061eda8a33ad1d40d68046b0927f3ab91
Karan Preet Singh Sasan <preetkaran20@gmail.com>
2022-07-22 19:43:22 +0530

    Update jest.config.ts

--- facade-app/jest.config.ts
+++ facade-app/jest.config.ts
@@ -3,6 +3,9 @@ const config = {
     transform: {
         "^.+\\.(ts|tsx|js|jsx)$": "ts-jest"
     }
+    testPathIgnorePatterns: [
+    '<rootDir>/src/interface/'
+    ]
 }
 
 module.exports = config;
